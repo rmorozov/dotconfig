7913f8513f21f14f8542cc818c919766dd292416
rmorozov <ruslan.y.morozov@gmail.com>
2026-09-22 16:44:24 +0300

    Exercise refresh PR helper in validation

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -25,6 +25,8 @@ for script in "${shell_scripts[@]}"; do
     bash -n "$script"
 done
 
+bash scripts/test-open-refresh-pr.sh
+
 for script in home/dot_config/zsh/*.zsh; do
     zsh -n "$script"
 done
