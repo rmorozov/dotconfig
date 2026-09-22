b1ce472d8a1e20cbe713efdee15de32a2da942dd
rmorozov <ruslan.y.morozov@gmail.com>
2026-09-22 16:10:32 +0300

    Repair package manifest validation tail

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -55,8 +55,6 @@ awk '
     $2 == "-" && $3 == "-" { exit 1 }
 ' packages/packages.tsv
 
-grep -Eq '^[0-9a-f]{40}
-echo "Static validation passed"
- versions/oh-my-zsh
+grep -Eq '^[0-9a-f]{40}$' versions/oh-my-zsh
 
 echo "Static validation passed"
