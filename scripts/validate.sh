f93cd6edfaa8a0a24ff1aeb23db3da6a1634c451
rmorozov <ruslan.y.morozov@gmail.com>
2026-09-25 17:54:51 +0300

    Add local dotconfig state report (#54)
    * Add local machine state report
    
    * Add local machine state report
    
    * Wire local state report into dotconfig
    
    * Wire local state report into dotconfig
    
    * Wire local state report into dotconfig

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -41,6 +41,7 @@ bash scripts/test-update-bootstrap-tools.sh
 bash scripts/test-mise-archive-checksum.sh
 bash scripts/test-chezmoi-archive-checksum.sh
 bash scripts/test-open-refresh-pr.sh
+bash scripts/test-report.sh
 bash scripts/test-update-oh-my-zsh.sh
 bash scripts/test-refresh-vim-bootstrap.sh
 bash scripts/test-update-vim-plug.sh
